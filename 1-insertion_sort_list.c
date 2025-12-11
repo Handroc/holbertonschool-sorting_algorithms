7be7a147317e874ebae311073759b5e36c914418
Handroc <andricassani42069@gmail.com>
2025-12-11 11:46:19 +0100

    fix: fix indentantion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -15,7 +15,7 @@ void sort_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 
 	if (node2->next)
 		node2->next->prev = node1;
-	
+
 	node2->prev = node1->prev;
 	node1->next = node2->next;
 	node1->prev = node2;
